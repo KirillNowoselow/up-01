9a30524f3ad146f0a1578cca8605e0b7e4accd65
xa4ipur4ik <kirillnowoselow4@gmail.com>
2023-03-08 18:30:10 +0300

    asdad

---
+++ mmtr/Log.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mmtr
+{
+    /// <summary>
+    /// Логика взаимодействия для Log.xaml
+    /// </summary>
+    public partial class Log : Window
+    {
+        public ObservableCollection<Group> Groups;
+        public Log()
+        {
+            InitializeComponent();
+            Group.CreateGroups();
+
+            using (var db = new DBContext())
+            {
+                db.Groups.ToList().ForEach(g => groups.Items.Add(g.GetCode()));
+            }
+        }
+    }
+}
